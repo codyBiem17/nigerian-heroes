4ed0ddd097d2f0f0bab0ab2c936fbcd271d6ede0
biem <bellomaryam@ymail.com>
2020-10-25 21:10:17 +0100

    prev-one

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,34 +1,25 @@
 import React from 'react';
-import GalleryData from './data';
+import GalleryData from './data'
 
+const imagesGallery = () => {
 
-const ImagesGallery = ({handleClick}) => {
-  // const [isClicked, setIsClicked] = useState(null);
-  // let history = useHistory();
     // let keys = Object.keys(GalleryData);
     // let randomKeys  = keys[Math.floor(Math.random() * keys.length)]
     // let randomObj = 
     const images = GalleryData.map((heroImg) => {
         return (
-          <div
-            className="imgWrapper tooltipWrapper"
-            key={heroImg.id}
-          >
-            <img
-              // id={heroImg.id}
-              src={heroImg.imgSrc}
-              alt="heroes-and-heroines"
-              onClick={() => { handleClick(heroImg.id) }}
-            />
-            <span className="tooltipName">{heroImg.name}</span>
-          </div>
-        );
+            <div className="imgWrapper" key={heroImg.id}>
+                <img src={heroImg.imgSrc} />
+            </div>
+        )
     })
 
-  return (
-    <div className="Img-Container grid-row">{images}</div>
-  ); 
+    return (
+        <div className="container">
+            {images}
+        </div>
+    ); 
 
 }
 
-export default ImagesGallery;
+export default imagesGallery;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/styles.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
